1d47f01acb98d1d40ec4c9636ca111747ede47d6
Francis Fajardo <francisdominic.fajardo@gmail.com>
2021-11-04 15:07:51 +0800

    Add skeleton common script

---
+++ assets/common.js
@@ -0,0 +1,14 @@
+var gSite = {
+    initialize: function () {
+    },
+
+    deferredLoad: function () {
+    },
+    
+    reflow: function () {
+    },
+};
+
+window.addEventListener("DOMContentLoaded", gSite.initialize);
+window.addEventListener("load", gSite.deferredLoad);
+window.addEventListener("resize", gSite.reflow);
