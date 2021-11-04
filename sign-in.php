84fd1f6012c5e09e2e136f1d7809c2fa8bd13db1
Francis Fajardo <francisdominic.fajardo@gmail.com>
2021-11-04 15:54:09 +0800

    Move sign-in to template

--- sign-in.php
+++ sign-in.php
@@ -1,32 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Sign In - KoDatos</title>
-        <link rel="shortcut icon" href="favicon.ico">
-        <link href="assets/common.css" rel="stylesheet">
-        <!-- <script src="assets/common.js" type="text/javascript"></script> -->
-    </head>
-    <body>
-        <main id="main">
-            <section id="section-login" class="bg ib0 vh-full box v ac jc c">
-                <img src="assets/images/logo.svg#sprite" width="300" height="100"/>
-                <div class="card l">
-                    <form method="post">
-                        <input type="email" id="field-email" class="mb1" name="field-email" placeholder="Email" required>
-                        <input type="password" id="field-password" class="mb1" name="field-password" placeholder="Password" required>
-                        <div class="box ac jc v">
-                            <button class="mb1">
-                                <span class="text c nm">Sign In</span>
-                            </button>
-                            <!-- <a href="#forgot"> -->
-                                Forgot your password?
-                            <!-- </a> -->
-                        </div>
-                    </form>
-                </div>
-            </section>
-        </main>
-    </body>
-</html>
+<?php
+
+require_once "./components/framework.php";
+
+$document = new Template("sign-in");
+
+$page_info = [];
+$page_info["TPL_HEADER"] = Utils::getTemplate("_header");
+
+$document->setData($page_info);
+
+echo $document->output();
+
+?>
