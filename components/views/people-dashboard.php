ce36b2ac5ea98050e6c52c59fdb7d990ae7103b4
Francis Fajardo <francisdominic.fajardo@gmail.com>
2021-11-06 22:15:52 +0800

    Implement status checking in people dashboard

--- components/views/people-dashboard.php
+++ components/views/people-dashboard.php
@@ -6,7 +6,7 @@ class PeopleDashboardView extends DashboardView
 
     public function getDocument()
     {
-        Framework::loadMultiple(["DBPAT"]); //, "DBLOC", "DBPAT", "DBPRO", "DBSIT", "DBWOR", "DBTSR", "DBTTY"]);
+        Framework::loadMultiple(["DBPAT", "DBTSR", "DBVAX"]);
 
         $document = parent::getDocument();
         $document->getDataByRef()["RGA_0"] = "selected";
@@ -21,6 +21,66 @@ class PeopleDashboardView extends DashboardView
             $record = $patients[$i];
             $cd_ref = $record["reference_code"];
 
+            $info = [];
+
+            $info["tests"] = DBM::$com["TSTR"]->readFromPatientId($record["id"]);
+            $tested = !is_bool($info["tests"]);
+            $tsr_color = "";
+            $tsr_icon = "";
+            $tsr_text = "";
+
+            if ($tested)
+            {
+                $test_result = $info["tests"][0];
+                if ((bool)$test_result["test_result"])
+                {
+                    $tsr_color = "red";
+                    $tsr_icon = "alert-circle";
+                    $tsr_text = "POSITIVE";
+                }
+                else
+                {
+                    $tsr_color = "green";
+                    $tsr_icon = "checkbox-marked-circle";
+                    $tsr_text = "NEGATIVE";
+                }
+            }
+            else
+            {
+                $tsr_color = "yellow";
+                $tsr_icon = "checkbox-marked-circle";
+                $tsr_text = "UNTESTED";
+            }
+
+            $info["vaxrecord"] = DBM::$com["VAXR"]->readFromPatientId($record["id"]);
+            $vaccinated = !is_bool($info["vaxrecord"]);
+            $vax_color = "";
+            $vax_icon = "";
+            $vax_text = "";
+
+            if ($vaccinated)
+            {
+                $vax_count = count($info["vaxrecord"]);
+                if ($vax_count > 1)
+                {
+                    $vax_color = "green";
+                    $vax_icon = "checkbox-marked-circle";
+                    $vax_text = "NEGATIVE";
+                }
+                else
+                {
+                    $vax_color = "yellow";
+                    $vax_icon = "checkbox-marked-circle";
+                    $vax_text = "UNTESTED";
+                }
+            }
+            else
+            {
+                $vax_color = "red";
+                $vax_icon = "alert-circle";
+                $vax_text = "POSITIVE";
+            }
+
             $name = sprintf(
                 "%s, %s %s",
                 strtoupper($record["last_name"]),
@@ -30,8 +90,18 @@ class PeopleDashboardView extends DashboardView
 
             $row = <<<EOD
 <div class="row">
-    <span class="cell left-col">$name</span>
-    <span class="cell box">
+    <span class="cell hr left-col">$name</span>
+    <div class="cell">
+        <div class="status-box $tsr_color mr1">
+            <span class="mdi-set mdi-$tsr_icon"></span>
+            <span>$tsr_text</span>
+        </div>
+        <div class="status-box $vax_color">
+            <span class="mdi-set mdi-$vax_icon"></span>
+            <span>$vax_text</span>
+        </div>
+    </div>
+    <div class="cell box">
         <a class="action-button" href="/view?rfc=$cd_ref">
             <div>
                 <span class="mdi-set mdi-eye"></span>
@@ -50,7 +120,7 @@ class PeopleDashboardView extends DashboardView
                 Delete
             </div>
         </a>
-    </span>
+    </div>
 </div>
 EOD;
             $child->append($row);
