70050702fee0337398177da259e324b20f6d80e2
Francis Fajardo <francisdominic.fajardo@gmail.com>
2021-11-07 21:45:19 +0800

    Fix typo

--- assets/pwa.js
+++ assets/pwa.js
@@ -2,7 +2,7 @@ self.addEventListener("install", function(event) {
     event.waitUntil(
         caches.open("sw-cache").then(function(cache) {
             return;
-        });
+        })
     );
 };
 
@@ -10,6 +10,6 @@ self.addEventListener("fetch", function(event) {
     event.respondWith(
         caches.match(event.request).then(function(response) {
             return response || fetch(event.request);
-        });
+        })
     );
 };
