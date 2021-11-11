1da2ec3ae11e8c6688c15b3664013b88e626ee49
Francis Fajardo <francisdominic.fajardo@gmail.com>
2021-11-11 22:10:50 +0800

    More dashboard navigation improvements

--- components/views/_dashboard.php
+++ components/views/_dashboard.php
@@ -30,6 +30,9 @@ class DashboardView extends BaseView
             "PAGE_NAME" => "Dashboard",
             "PAGE_MARKER" => "dashboard",
             "PAGE_HEADER_STATIC" => "true",
+            "DASHBOARD_RECORDS_VISIBLE" => "",
+            "DASHBOARD_MAPPING_VISIBLE" => (Auth::getRoleID() > 2) ? "hidden" : "",
+            "DASHBOARD_ADMIN_VISIBLE"   => (Auth::getRoleID() > 1) ? "hidden" : "",
         ]);
 
         $this->mainTpl = new Template("_dashboard");
