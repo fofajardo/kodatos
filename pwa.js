d46bc39baf4722ebb38f7881c7e06d12e058238f
Francis Fajardo <francisdominic.fajardo@gmail.com>
2021-11-11 17:05:53 +0800

    [PWA] Service worker improvements
    - Add offline page handling
    - Add support for preloading

--- pwa.js
+++ pwa.js
@@ -1,45 +1,72 @@
-const version = "1.2.0";
-const cacheName = `kodatos-${version}`;
+const CACHE_VERSION = "1.3.0";
+const CACHE_NAME = `kodatos-${CACHE_VERSION}`;
 
-const cachedAssets = [
-    `/`, 
-    `/enter-code`, 
-    `/assets/images/404.svg`, 
-    `/assets/images/code.svg`, 
-    `/assets/images/taken.svg`, 
-    `/assets/images/sections/0.jpg`, 
-    `/assets/images/sections/1.jpg`, 
-    `/assets/branding/logo.svg`, 
-    `/assets/branding/logo-full.svg`, 
-    `/assets/common.css`, 
-    `/assets/common.proto.css`, 
-    `/assets/common.js`, 
-    `/assets/qrcode.min.js`, 
+const OFFLINE_URL = "/pwa/offline";
+const CACHE_ASSETS = [
+    `/`,
+    `/enter-code`,
+    `${OFFLINE_URL}`,
+    `/assets/images/404.svg`,
+    `/assets/images/code.svg`,
+    `/assets/images/taken.svg`,
+    `/assets/images/void.svg`,
+    `/assets/images/sections/0.jpg`,
+    `/assets/images/sections/1.jpg`,
+    `/assets/branding/logo.svg`,
+    `/assets/branding/logo-full.svg`,
+    `/assets/common.css`,
+    `/assets/common.proto.css`,
+    `/assets/common.js`,
+    `/assets/qrcode.min.js`,
     `/assets/qrcode-scan.min.js`
 ];
 
 self.addEventListener("install", e => {
     e.waitUntil(
-        caches.open(cacheName)
+        caches.open(CACHE_NAME)
               .then(cache => {
-            return cache.addAll(cachedAssets)
+            return cache.addAll(CACHE_ASSETS)
                         .then(() => self.skipWaiting());
         })
     );
 });
 
 self.addEventListener("activate", event => {
-    event.waitUntil(self.clients.claim());
+    event.waitUntil((async () => {
+        if ("navigationPreload" in self.registration) {
+            await self.registration.navigationPreload.enable();
+        }
+    })());
+
+    self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.open(cacheName).then(cache =>
-            cache.match(event.request, {
-                ignoreSearch: true
-            })
-        ).then(response => {
-            return response || fetch(event.request);
-        })
-    );
+    event.respondWith((async () => {
+        const cache = await caches.open(CACHE_NAME);
+
+        const cachedResponse = await cache.match(
+            event.request,
+            { ignoreSearch: true }
+        );
+        if (cachedResponse) {
+            return cachedResponse;
+        }
+
+        const preloadResponse = await event.preloadResponse;
+        if (preloadResponse) {
+            return preloadResponse;
+        }
+
+        try {
+            const networkResponse = await fetch(event.request);
+            return networkResponse;
+        } catch (e) {
+            if (event.request.mode === "navigate") {
+                const offlineResponse = await cache.match(OFFLINE_URL);
+                return offlineResponse;
+            }
+            return e;
+        }
+    })());
 });
