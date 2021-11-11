4a9d203d5752a64f79732dfeee55170945e66843
Francis Fajardo <francisdominic.fajardo@gmail.com>
2021-11-11 16:07:39 +0800

    [PWA] Move assets into its own directory
    - In preparation for other PWA-related work

--- pwa.js
+++ pwa.js
@@ -1,33 +1,38 @@
 const version = "1.2.0";
 const cacheName = `kodatos-${version}`;
-self.addEventListener('install', e => {
+
+const cachedAssets = [
+    `/`, 
+    `/enter-code`, 
+    `/assets/images/404.svg`, 
+    `/assets/images/code.svg`, 
+    `/assets/images/taken.svg`, 
+    `/assets/images/branding/logo.svg`, 
+    `/assets/images/branding/logo-full.svg`, 
+    `/assets/images/sections/0.jpg`, 
+    `/assets/images/sections/1.jpg`, 
+    `/assets/common.css`, 
+    `/assets/common.proto.css`, 
+    `/assets/common.js`, 
+    `/assets/qrcode.min.js`, 
+    `/assets/qrcode-scan.min.js`
+];
+
+self.addEventListener("install", e => {
     e.waitUntil(
-        caches.open(cacheName).then(cache => {
-            return cache.addAll([
-                `/`, 
-                `/enter-code`, 
-                `/assets/images/404.svg`, 
-                `/assets/images/code.svg`, 
-                `/assets/images/taken.svg`, 
-                `/assets/images/branding/logo.svg`, 
-                `/assets/images/branding/logo-full.svg`, 
-                `/assets/images/sections/0.jpg`, 
-                `/assets/images/sections/1.jpg`, 
-                `/assets/common.css`, 
-                `/assets/common.proto.css`, 
-                `/assets/common.js`, 
-                `/assets/qrcode.min.js`, 
-                `/assets/qrcode-scan.min.js`
-            ]).then(() => self.skipWaiting());
+        caches.open(cacheName)
+              .then(cache => {
+            return cache.addAll(cachedAssets)
+                        .then(() => self.skipWaiting());
         })
     );
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener("activate", event => {
     event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", event => {
     event.respondWith(
         caches.open(cacheName).then(cache =>
             cache.match(event.request, {
